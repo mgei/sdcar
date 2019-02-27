2d73dc1823325b935facd10022f230e8b7a79b6d
Martin <martin.geissmann@gmail.com>
2019-02-27 08:26:52 +0100

    motorctrl pygame

--- myownmotorctrl.py
+++ myownmotorctrl.py
@@ -1,27 +1,96 @@
 import RPi.GPIO as GPIO
 import time
+import pygame
+from pygame.locals import *
+import sys
 
-GPIO.setmode(GPIO.BOARD)
+class Car:
+    def __init__(self, fstate, bstate, rstate, lstate):
+        self.fstate = 0
+        self.bstate = 0
+        self.rstate = 0
+        self.lstate = 0
 
-GPIO.setup(22, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
+    def forward(self):
+        if self.fstate == 0:
+            print("going forward")
+            self.fstate = 1
+            
+    def backward(self):
+        if self.bstate == 0:
+            print("backward")
+            self.bstate = 1
+    
+    def right(self):
+        if self.rstate == 0:
+            print("right")
+            self.rstate = 1
 
-pwm=GPIO.PWM(22, 100)
+    def left(self):
+        if self.lstate == 0:
+            print("left")
+            self.lstate = 1
 
-pwm.start(0)
+pygame.display.set_caption("Empty Pygame")
+screen = pygame.display.set_mode([640,480])
 
-GPIO.output(18, True)
-GPIO.output(16, False)
+carryOn = 1
 
-pwm.ChangeDutyCycle(80)
+while carryOn:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            carryOn=0
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_x:     # press X to exit
+                carryOn = 0
 
-GPIO.output(22, True)
+    key_input = pygame.key.get_pressed()
+    
+    if key_input[pygame.K_UP] and key_input[pygame.K_RIGHT]:
+        print("Forward Right")
+    elif key_input[pygame.K_UP] and key_input[pygame.K_LEFT]:
+        print("Forward Left")
+    elif key_input[pygame.K_DOWN] and key_input[pygame.K_RIGHT]:
+        print("Reverse Right")
+    elif key_input[pygame.K_DOWN] and key_input[pygame.K_LEFT]:
+        print("Reverse Left")
+    elif key_input[pygame.K_UP]:
+        print("Forward")
+    elif key_input[pygame.K_DOWN]:
+        print("Reverse")
+    elif key_input[pygame.K_LEFT]:
+        print("left")
+    elif key_input[pygame.K_RIGHT]:
+        print("right")
+    else:
+        print("no key")
 
-time.sleep(2)
+    time.sleep(0.1)
 
-GPIO.output(22, False)
 
-pwm.stop()
-GPIO.cleanup()
 
+
+#GPIO.setmode(GPIO.BOARD)
+#
+#GPIO.setup(22, GPIO.OUT)
+#GPIO.setup(18, GPIO.OUT)
+#GPIO.setup(16, GPIO.OUT)
+#
+#pwm=GPIO.PWM(22, 100)
+#
+#pwm.start(0)
+#
+#GPIO.output(18, True)
+#GPIO.output(16, False)
+#
+#pwm.ChangeDutyCycle(80)
+#
+#GPIO.output(22, True)
+#
+#time.sleep(2)
+#
+#GPIO.output(22, False)
+#
+#pwm.stop()
+#GPIO.cleanup()
+#
